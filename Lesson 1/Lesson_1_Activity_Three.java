b7e1be89e6edebdc783ea178ac9b2b24603b818d
miryokuu <39278009+miryokuu@users.noreply.github.com>
2019-08-16 17:51:41 -0700

    init commit

---
+++ Lesson 1/Lesson_1_Activity_Three.java
@@ -0,0 +1,17 @@
+/*
+ * Lesson 1 Coding Activity Question 3
+ * 
+ * Write a program to output the following:
+
+    ********
+    * java *
+    ********
+
+ * For an extra challenge: complete this problem using only one line of code.
+*/
+
+class Lesson_1_Activity_Three {
+    public static void main(String[] args) {
+        System.out.println("********\n* java *\n********");
+    }
+}
---
+++ Lesson 2/Lesson_2_Activity_One.java
@@ -0,0 +1,16 @@
+/*
+ * Lesson 2 Coding Activity Question 1
+ * 
+ * Print the following shape:
+
+    /\
+    \/
+	
+ * (Note you will have to use escape characters to make this work.)
+*/
+
+class Lesson_2_Activity_One {
+    public static void main(String[] args) {
+        System.out.println("/\\\n\\/");
+    }
+}
