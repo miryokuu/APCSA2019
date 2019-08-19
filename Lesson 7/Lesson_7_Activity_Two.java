9452475d5a944d0f5fc1b33e45d214c21da22ac5
miryokuu <39278009+miryokuu@users.noreply.github.com>
2019-08-19 13:18:30 -0700

    7-2 complete

--- Lesson 7/Lesson_7_Activity_Two.java
+++ Lesson 7/Lesson_7_Activity_Two.java
@@ -25,12 +25,29 @@ import java.lang.Math;
 
 class Lesson_7_Activity_Two {
     public static void main(String[] args) {
-      
-        /* Write your code here 
-         * Copy and paste your entire code to Code Runner to complete the activity, 
-         * from the first import statement to the last bracket. 
-        */
-
-
+        Scanner input = new Scanner(System.in);
+        int num, result1, result2, result3;
+        
+        num = input.nextInt();
+        
+        // if(num > 999 || num < 100) { input.close(); throw new RuntimeException("I said three positive integer.");} 
+        /*
+         * Runtime Error
+         * 
+         * Exception in thread "main" java.lang.RuntimeException: I said three positive digits.
+         *  at Lesson_7_Activity_One.main(Main.java:262)
+         *  at Ideone.assertRegex(Main.java:88)
+         *  at Ideone.test(Main.java:40)
+         *  at Ideone.main(Main.java:29)
+         *  
+         *  You lied to me AGAIN you said THREE DIGIT INTEGER
+         */
+        result1 = (int)(num / 100);
+        result2 = (int)((num % 100) / 10);
+        result3 = (num % 10);
+        
+        System.out.println(result1 + "\n" + result2 + "\n" + result3);
+        System.out.println(result1 + " + " + result2 + " + " + result3 + " = " + (result1+result2+result3));
+        input.close();
     }
 }
