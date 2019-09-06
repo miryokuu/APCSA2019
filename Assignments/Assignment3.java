7d66649136363a1d3ec705e8713a64cb32d8aa64
miryokuu <39278009+miryokuu@users.noreply.github.com>
2019-09-05 21:45:04 -0700

    soon

---
+++ Assignments/Assignment3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/*
+ * Rename this class to Main!
+ */
+public class Assignment3 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+    }
+}
