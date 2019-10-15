9ab75e93cef7fdaaadb4345b23fbe46fbca23fca
miryokuu <39278009+miryokuu@users.noreply.github.com>
2019-10-15 13:14:37 -0700

    my pc about to run out of battery

---
+++ Term 2/Assignments/Student.java
@@ -0,0 +1,21 @@
+
+public class Student {
+    
+    public String firstName, lastName;
+    public int gradeLevel;
+    public double GPA;
+    
+    Student()
+    {
+        
+    }
+    
+    Student(String firstName, String lastName, int gradeLevel, double gpa)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gradeLevel = gradeLevel;
+        this.GPA = gpa;
+    }
+
+}
