930466710b7d8eda7f38d1aa6cd0390542d9ddf3
miryokuu <39278009+miryokuu@users.noreply.github.com>
2019-10-15 13:24:29 -0700

    aagsaf

--- Term 2/Assignments/Student.java
+++ Term 2/Assignments/Student.java
@@ -1,21 +1,32 @@
 
 public class Student {
-    
+    public static int studentID = 0;
     public String firstName, lastName;
     public int gradeLevel;
     public double GPA;
     
     Student()
     {
-        
+        this.firstName = "None";
+        this.lastName = "None";
+        this.gradeLevel = 0;
+        this.GPA = 0d;
+        Student.studentID++;
     }
     
     Student(String firstName, String lastName, int gradeLevel, double gpa)
     {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.gradeLevel = gradeLevel;
-        this.GPA = gpa;
+        this.gradeLevel = gradeLevel >= 0 && gradeLevel <= 12 ? gradeLevel : 0;
+        this.GPA = gpa >= 0.0 && gpa <= 4.5 ? gpa : 0d;
+        Student.studentID++;
+    }
+    
+    @Override
+    public String toString() 
+    {
+        return String.format("%s, %s\nGPA: %f\nGrade Level: %d id # %d", this.firstName, this.lastName);
     }
 
 }
