2fb226fd5e5bc0040e65c11da0b6acd42707b281
miryokuu <39278009+miryokuu@users.noreply.github.com>
2019-10-25 14:04:53 -0700

    incomplete assignment4

--- Term 2/Assignments/Fraction.java
+++ Term 2/Assignments/Fraction.java
@@ -1,8 +1,14 @@
+
 /* Term 2 Assignment 1 - Fraction */
 /* A class which is used to represent fractions*/
-public class Fraction {
-    private int numerator;
-    private int denominator;
+public class Fraction implements java.lang.Comparable<Object> {
+    public int numerator;
+    public int denominator;
+    
+    /**
+     * you should disable this if on assignment 1.
+     */
+    public static final boolean ASSIGNMENT_4 = true;
 
     /**
      * default constructor which creates a fraction 1/1
@@ -24,6 +30,9 @@ public class Fraction {
     {
         this.numerator = n > 0 ? n : 1;
         this.denominator = d > 0 ? d : 1;
+        
+        if(ASSIGNMENT_4)
+            simplify();
     }
 
     /**
@@ -64,5 +73,54 @@ public class Fraction {
         
         this.numerator = this.numerator * d + n * this.denominator;
         this.denominator *= d; 
+    }
+    
+    /*
+     * Assignment 4 parts
+     */
+    
+    /**
+     * return the greatest common divisor of a and b. 
+     * To find the gcd of two numbers there are several possible algorithms. 
+     * One of these was showcased in term 1 lesson 19 - More Loops.
+     * 
+     * @param a
+     * @param b
+     * @return greatest common divisor
+     */
+    static int gcd(int a, int b) 
+    {
+        while(b != 0){
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+    
+    /**
+     * void simplify(): reduce the fraction to its simplest possible form: 
+     * e.g. the fraction 12/18 should be reduced to 2/3. 
+     * The static method gcd provides a useful helper for this method.
+     */
+    void simplify() 
+    {
+        numerator = numerator / gcd(numerator, denominator);
+        denominator = denominator / gcd(numerator, denominator);
+    }
+
+    /**
+     * Return -1 if this fraction is smaller than the fraction other, 
+     * return 0 if this fraction is equal to the fraction other, 
+     * and return 1 if this fraction is greater than the fraction other. 
+     * Hint: To compare the two fractions, it will help to first convert them to fractions with a common (equal) denominator.
+     *
+     * @return 0|1
+     */
+    @Override
+    public int compareTo(Object other)
+    {
+        Fraction target = (Fraction)other;
+        return 0;
     }
 }
--- Term 2/Assignments/student_fraction_runner.java
+++ Term 2/Assignments/student_fraction_runner.java
@@ -2,7 +2,14 @@ import java.io.IOException;
 
 public class student_fraction_runner {
 
-    public static void main(String str[]) throws IOException
+    public static void main(String str[]) throws IOException 
+    {
+        if (main_1(str))
+            main_2(str);
+    }
+    
+    
+    public static boolean main_1(String str[]) throws IOException
     {
         Fraction fraction1 = new Fraction(7, 4);
         System.out.println("Fraction 1: " + fraction1);
@@ -35,6 +42,33 @@ public class student_fraction_runner {
         System.out.println("Add 2/3: " + fraction5);
         System.out.println("As a mixed number: " + fraction5.mixedNumber());
         System.out.println();
-
+        
+        return Fraction.ASSIGNMENT_4;
+    }
+    
+    public static void main_2(String str[]) throws IOException {
+        
+        System.out.println(Fraction.gcd(100, 10));
+        
+        Fraction fraction1 = new Fraction(8, 10);
+        System.out.println("Fraction 1: " + fraction1);
+        
+        Fraction fraction2 = new Fraction(12, 8);
+        System.out.println("Fraction 2: " + fraction2);
+        
+        Fraction fraction3 = new Fraction(4, 5);
+        System.out.println("Fraction 3: " + fraction3);
+        System.out.println();
+        
+        System.out.println("Compare fraction 1 to fraction 2: " + fraction1.compareTo(fraction2));
+        System.out.println("Compare fraction 2 to fraction 1: " + fraction2.compareTo(fraction1));
+        System.out.println();
+        
+        System.out.println("Compare fraction 1 to fraction 3: " + fraction1.compareTo(fraction3));
+        System.out.println("Compare fraction 3 to fraction 1: " + fraction3.compareTo(fraction1));
+        System.out.println();
+        
+        System.out.println("Compare fraction 2 to fraction 3: " + fraction2.compareTo(fraction3));
+        System.out.println("Compare fraction 3 to fraction 2: " + fraction3.compareTo(fraction2));
     }
 }
